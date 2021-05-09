9f8510e9723a9cfd5243e3fcb31f2ec0762b1b39
Orr112 <omarr112@hotmail.com>
2021-05-09 16:59:09 -0500

    BigQuery Update

--- dbt_demo/models/example/jaffle_shop.sql
+++ dbt_demo/models/example/jaffle_shop.sql
@@ -1,63 +1,56 @@
 with customers as (
 
-	Select
-		id as customer_id,
-		first_name,
-		last_name
+    select
+        id as customer_id,
+        first_name,
+        last_name
 
-	from 'dbt-tutorial'.jaffle_shop.customers
+    from `dbt-tutorial`.jaffle_shop.customers
 
 ),
 
-
 orders as (
 
-	Select
-		id as order_id,
-		user_id as customer_id,
-		order_date,
-		status
+    select
+        id as order_id,
+        user_id as customer_id,
+        order_date,
+        status
 
-	from 'dbt-tutorial'.jaffle_shop.orders
+    from `dbt-tutorial`.jaffle_shop.orders
 
+),
 
-)
+customer_orders as (
 
+    select
+        customer_id,
 
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders
 
-cutomer_orders as (
+    from orders
 
-	Select
-		customer_id,
-
-		min(order_date) as first_order_date,
-		max(order_date) as most_recent_order_date,
-		count(order_id) as number_of_orders
-
-	from orders
-
-	group by1
-
-
+    group by 1
 
 ),
 
 
 final as (
 
-	Select
-		customers.customer_id,
-		customers.first_name,
-		customers.last_name,
-		customer_orders.first_order_date,
-		customer_oders.most_recent_order_date,
-		coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+    select
+        customers.customer_id,
+        customers.first_name,
+        customers.last_name,
+        customer_orders.first_order_date,
+        customer_orders.most_recent_order_date,
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
-	from customers
+    from customers
 
-	left join customer_orders using (customer_id)
-
+    left join customer_orders using (customer_id)
 
 )
 
-select * from final 
+select * from final
